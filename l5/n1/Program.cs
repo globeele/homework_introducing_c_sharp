c035d2b0c416e84d844b3f5dd3312c0469aeaab9
globeele <nsetko@yahoo.com>
2023-04-26 19:12:50 +0300

    l5 n1 completed

--- l5/n1/Program.cs
+++ l5/n1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArrayRandomarray(array);
+Console.WriteLine("Вывод массива: ");
+PrintArray(array);
+int count = 0;
+
+for (int z = 0; z < array.Length; z++)
+if (array[z] % 2 == 0)
+count++;
+
+Console.WriteLine("из " + array.Length + " чисел " + count + " четных");
+
+void FillArrayRandomarray(int[] array)
+    {
+        for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(100,1000);
+            }
+    }
+void PrintArray(int[] array)
+    {
+        Console.Write("[ ");
+        for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        Console.Write("]");
+        Console.WriteLine();
+    }
