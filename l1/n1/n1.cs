964ef0bb13b607cd57a381b0dda50894a5014a65
globeele <nsetko@yahoo.com>
2023-04-26 12:34:11 +0300

    n1 completed

--- l1/n1/n1.cs
+++ l1/n1/n1.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+int max = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    if(firstNumber > secondNumber)
+        {
+            max = firstNumber;
+        }
+    else
+        {
+            max = secondNumber;
+        }
+
+Console.WriteLine("max = " + max);
