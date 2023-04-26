f9c8f24ea3e43acb793f98880ff8ba31a78afb5d
globeele <nsetko@yahoo.com>
2023-04-26 13:17:49 +0300

    n1 complete

--- l2/n1/Program.cs
+++ l2/n1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string strnum = Convert.ToString(num);
+Console.WriteLine("Вторая цифра этого числа -> "+strnum[1]);
